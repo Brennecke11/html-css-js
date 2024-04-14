12d5d0fa1e385a6741289b3a28de2d4352ca3e97
Guilherme Brennecke Trova <87507732+Brennecke11@users.noreply.github.com>
2024-04-14 14:07:11 -0300

    Update register.php

--- register.php
+++ register.php
@@ -1,1 +1,40 @@
 
+<?php
+$host = '';
+$db   = 'db_teste';
+$user = 'root'; // padrão do XAMPP
+$pass = '';     // padrão do XAMPP
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (nome, email, senha) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sss", $nome, $email, $senha);
+
+    if ($stmt->execute()) {
+        echo "Cadastro realizado com sucesso!";
+        
+        // Redirecionar para a página de login após 3 segundos
+        header("Location: http://localhost/projetos/login.html");
+        exit(); // Encerra o script após o redirecionamento
+ 
+	} else {
+    // Redirecionar para o formulário de cadastro
+    header("Location: register-form.html");
+    exit();
+	}
+
+
+    $stmt->close();
+    $conn->close();
+}
+?>
